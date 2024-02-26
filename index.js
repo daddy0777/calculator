f7c3c60b5916122aa933985861dd005b93375927
daddy0777 <boatt54489@gmail.com>
2024-02-26 23:28:47 +0530

    commit

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 // let btn = document.getElementById("btn");
+
 // btn.addEventListener("click",()=>{
 //     let div  = document.createElement("div");
 //     let label1 = document.createElement("label");
